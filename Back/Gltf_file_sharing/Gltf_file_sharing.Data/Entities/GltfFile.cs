c0cc7d537b6bc5be30119d144eef2ce7f1cab6a1
IvanKalimanov <ikalimanov@mail.ru>
2020-11-07 11:24:12 +0300

    Gltf_file_sharing.API and Gltf_file_sharing.Data projects were created in solution

---
+++ Back/Gltf_file_sharing/Gltf_file_sharing.Data/Entities/GltfFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gltf_file_sharing.Data.Entities
+{
+    public class GltfFile
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Path { get; set; }
+
+        public DateTime CreationDateTime { get; set; }
+
+        public DateTime LastUpdateDateTime { get; set; }
+    }
+}
---
+++ Back/Gltf_file_sharing/Gltf_file_sharing.Data/Repositories/IGltfFileRepository.cs
@@ -0,0 +1,22 @@
+﻿using Gltf_file_sharing.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gltf_file_sharing.Data.Repositories
+{
+    public interface IGltfFileRepository
+    {
+        Task<GltfFile> GetById(Guid id);
+
+        Task<ICollection<GltfFile>> Get();
+
+        Task<GltfFile> Create(GltfFile gltfFile);
+
+        Task<bool> Delete(Guid id);
+
+        Task<GltfFile> Update(GltfFile gltfFile);
+
+    }
+}
