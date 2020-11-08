bbbd759d7b250f40fd75123dad67763933b6cb88
AlexandrNemashkalo <60902736+AlexandrNemashkalo@users.noreply.github.com>
2020-11-08 21:03:14 +0300

    создал и настроил начальный проект

---
+++ Back/HSE.WebAR/Configurations/ServicesConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HSE.WebAR.API.Configurations
+{
+    public class ServicesConfiguration
+    {
+    }
+}
---
+++ Back/HSE.WebAR/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HSE.WebAR.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestController :Controller
+    {
+
+        [HttpGet]
+        public string GetStringStatus()
+        {
+                return "заебись, все работает"; 
+        }
+    }
+}
